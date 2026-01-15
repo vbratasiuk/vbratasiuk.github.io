dd9ef891619b1cbded4db6cac230e598e91341ed
Bratasiuk, Volodymyr <vbratasiuk@deloitte.es>
2026-01-15 14:53:12 +0100

    remodelation

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/vbratasiuk.github.io/"
+  base: 'https://vbratasiuk.github.io/', // Link page
 })
